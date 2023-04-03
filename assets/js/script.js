44a06153806b134e2a6e5d1d4883d6b4d8b58c44
James McConnaughey <jbmcconnaughey@gmail.com>
2023-04-02 23:26:20 -0400

    got the 5 day forecast mostly written - not as clever as a for loop function, but I like the formatting of the info

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,7 +25,7 @@ fetch(queryURL)
     console.log('Weather data for '+city);
     console.log(data);
     let unix_timestamp = data.dt;
-    var date = new Date(unix_timestamp *1000).toLocaleDateString('en-us', { weekday:"long", year:"numeric", month:"short", day:"numeric"});
+    var date = new Date(unix_timestamp *1000).toLocaleDateString('en-us', { weekday:"long", year:"numeric", month:"long", day:"numeric", hour:"2-digit"});
     //var formattedDate = date.getDate();
     
     weatherToday.children[0].textContent = data.name;
@@ -53,6 +53,34 @@ fetch(fiveDayqueryURL)
 .then(function(data){
     console.log('5 day Weather data for '+city);
     console.log(data);
+
+    let unix_timestamp1 = data.list[6].dt;
+    var date1 = new Date(unix_timestamp1 *1000).toLocaleDateString('en-us', { weekday:"short", month:"numeric", day:"numeric"});
+    console.log(date1);
+    fiveDayForecast.children[0].children[0].textContent = date1;
+    fiveDayForecast.children[0].children[1].textContent = "Temp: "+data.list[6].main.temp+" °F";
+    fiveDayForecast.children[0].children[2].textContent = "Wind: "+data.list[6].wind.speed +" mph";
+    fiveDayForecast.children[0].children[3].textContent = "Humidity: "+data.list[6].main.humidity+"%";
+
+    let unix_timestamp2 = data.list[14].dt;
+    var date2 = new Date(unix_timestamp2 *1000).toLocaleDateString('en-us', { weekday:"short", month:"numeric", day:"numeric"});
+    console.log(date2);
+    fiveDayForecast.children[1].children[0].textContent = date2;
+
+    let unix_timestamp3 = data.list[22].dt;
+    var date3 = new Date(unix_timestamp3 *1000).toLocaleDateString('en-us', { weekday:"short", month:"numeric", day:"numeric"});
+    console.log(date3);
+    fiveDayForecast.children[2].children[0].textContent = date3;
+
+    let unix_timestamp4 = data.list[30].dt;
+    var date4 = new Date(unix_timestamp4 *1000).toLocaleDateString('en-us', { weekday:"short", month:"numeric", day:"numeric"});
+    console.log(date4);
+    fiveDayForecast.children[3].children[0].textContent = date4;
+
+    let unix_timestamp5 = data.list[38].dt;
+    var date5 = new Date(unix_timestamp5 *1000).toLocaleDateString('en-us', { weekday:"short", month:"numeric", day:"numeric"});
+    console.log(date5);
+    fiveDayForecast.children[4].children[0].textContent = date5;
 
 }
 )
